94329ceabcf796770f4e44fe2f3d6f69a4d12389
Marcos Vinícius <viniciuustl7@gmail.com>
2024-12-16 16:00:42 -0300

    feat✨: adicionar rota POST para salvar dados no servidor

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/perguntar", (req, res) => {
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/question", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/savequestion", (req, res) => {
+    res.send("Formulário recebido!")
 });
 
 app.listen(8000, () => {
   console.log("App rodando!");
 });
-
