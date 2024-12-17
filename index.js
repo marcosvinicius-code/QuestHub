2c278c8a8960fb336a16633498cb8c4ace82888c
Marcos Vinícius <viniciuustl7@gmail.com>
2024-12-17 15:53:38 -0300

    feat✨: integração modelo Question no index.js e conexão ao Sequelize 🔗

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/db.js");
+const questionModel = require("./database/Question.js");
 
+// Database Sequelize
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com banco de dados!");
+  })
+  .catch(() => {
+    console.log(msgErro);
+  });
+
+// Config EJS
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// Config body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Rotas
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -13,7 +32,11 @@ app.get("/question", (req, res) => {
 });
 
 app.post("/savequestion", (req, res) => {
-    res.send("Formulário recebido!")
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+  res.send(
+    "Formulário recebido! titulo " + titulo + " " + " descricao " + descricao
+  );
 });
 
 app.listen(8000, () => {
